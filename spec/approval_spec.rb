88db063633fbfb7715f4cedea9cfd1a853883bf7
Katrina Owen <katrina.owen@gmail.com>
2012-02-11 15:51:57 +0100

    Refactor approval specs

--- spec/approval_spec.rb
+++ spec/approval_spec.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
 
 describe Approvals::Approval do
-  let(:description) { 'spec/approvals/fairy_dust_and_unicorns' }
-  let(:example) { stub('example', :full_description => 'fairy dust and unicorns').as_null_object }
-
   describe "#normalize" do
     it "downcases" do
       Approvals::Approval.normalize("KTHXBYE").should eq("kthxbye")
@@ -34,148 +31,138 @@ The::Class       \t \r\n \fname
     end
   end
 
-  it "knows the approved_path" do
-    approval = Approvals::Approval.new(example)
-    approval.approved_path.should eq("#{description}.approved.txt")
-  end
+  let(:description) { 'spec/approvals/fairy_dust_and_unicorns' }
+  let(:example) { stub('example', :full_description => 'fairy dust and unicorns').as_null_object }
 
-  it "knows the received path" do
-    approval = Approvals::Approval.new(example)
-    approval.received_path.should eq("#{description}.received.txt")
-  end
+  describe "an approval" do
+    subject { Approvals::Approval.new(example) }
+    its(:approved_path) { should eq("#{description}.approved.txt") }
+    its(:received_path) { should eq("#{description}.received.txt") }
 
-  it "can set a location" do
-    Dir.stub(:pwd => 'the/path')
-    approval = Approvals::Approval.new(example)
-    approval.location = ['the/path/to/my/heart:9372 <is through my stomach>', 'bla bla bla']
-    approval.location.should eq(['./to/my/heart:9372 <is through my stomach>'])
-  end
-
-  context "approvals" do
-    before :each do
-      @approved_file = "#{description}.approved.txt"
-      @received_file = "#{description}.received.txt"
+    it "can set a location" do
+      Dir.stub(:pwd => 'the/path')
+      subject.location = ['the/path/to/my/heart:9372 <is through my stomach>', 'bla bla bla']
+      subject.location.should eq(['./to/my/heart:9372 <is through my stomach>'])
     end
+  end
+
+  describe "on the filesystem" do
+    let(:approved_file) { "#{description}.approved.txt" }
+    let(:received_file) { "#{description}.received.txt" }
 
     after :each do
-      File.delete(@approved_file) if File.exists?(@approved_file)
-      File.delete(@received_file) if File.exists?(@received_file)
+      File.delete(approved_file) if File.exists?(approved_file)
+      File.delete(received_file) if File.exists?(received_file)
+    end
+    it "writes the approved file if it doesn't exist" do
+      File.delete(approved_file) if File.exists?(approved_file)
+
+      Approvals::Approval.new(example)
+
+      File.exists?(approved_file).should be_true
+      File.read(approved_file).should eq('')
     end
 
-    describe "on the filesystem" do
-
-      it "writes the approved file if it doesn't exist" do
-        File.delete(@approved_file) if File.exists?(@approved_file)
-
-        Approvals::Approval.new(example)
-
-        File.exists?(@approved_file).should be_true
-        File.read(@approved_file).should eq('')
+    it "doesn't overwrite an existing approved file" do
+      File.open(approved_file, 'w') do |f|
+        f.write "this doesn't get deleted"
       end
 
-      it "doesn't overwrite an existing approved file" do
-        File.open(@approved_file, 'w') do |f|
-          f.write "this doesn't get deleted"
-        end
+      Approvals::Approval.new(example)
 
-        Approvals::Approval.new(example)
-
-        File.exists?(@approved_file).should be_true
-        File.read(@approved_file).should eq("this doesn't get deleted")
-      end
-
-      it "writes the received contents to file" do
-        approval = Approvals::Approval.new(example, 'oooh, shiney!')
-
-        File.exists?(@received_file).should be_true
-        File.read(@received_file).should eq('"oooh, shiney!"')
-      end
+      File.exists?(approved_file).should be_true
+      File.read(approved_file).should eq("this doesn't get deleted")
     end
 
-    describe "verification" do
+    it "writes the received contents to file" do
+      approval = Approvals::Approval.new(example, 'oooh, shiney!')
 
-      context "with a match" do
-        before :each do
-          @approval = Approvals::Approval.new(example, 'xyz')
-          @approval.write(:approved, 'xyz')
-        end
+      File.exists?(received_file).should be_true
+      File.read(received_file).should eq('"oooh, shiney!"')
+    end
+  end
 
-        it "does not raise an error" do
-          lambda { @approval.verify }.should_not raise_error(RSpec::Approvals::ReceivedDiffersError)
-        end
+  describe "verification" do
+    let(:approval) { Approvals::Approval.new(example, 'xyz') }
 
-        it "does not leave a received file" do
-          lambda { @approval.verify }.call
-          File.exists?(@approval.received_path).should be_false
-        end
-
-        it "deletes a previously rejected change from the dotfile" do
-          expected = "spec/approvals/fairy_dust_and_unicorns.received.txt spec/approvals/fairy_dust_and_unicorns.approved.txt"
-
-          @approval.write(:approved, 'abc')
-          begin
-            @approval.verify
-          rescue RSpec::Approvals::ReceivedDiffersError => e
-            # guard
-            File.read('.approvals').split("\n").should include(expected)
-          end
-
-          @approval.write(:approved, 'xyz')
-          lambda { @approval.verify }.call
-          File.read('.approvals').split("\n").should_not include(expected)
-        end
+    context "with a match" do
+      before :each do
+        approval.write(:approved, 'xyz')
       end
 
-      context "with a mismatch" do
-        before :each do
-          @approval = Approvals::Approval.new(example, 'xyz')
-          @approval.write(:approved, 'abc')
-        end
+      it "does not raise an error" do
+        lambda { approval.verify }.should_not raise_error(RSpec::Approvals::ReceivedDiffersError)
+      end
 
-        it "raises an error" do
-          lambda { @approval.verify }.should raise_error(RSpec::Approvals::ReceivedDiffersError)
-        end
+      it "does not leave a received file" do
+        lambda { approval.verify }.call
+        File.exists?(approval.received_path).should be_false
+      end
 
-        it "leaves a received file" do
-          begin
-            @approval.verify
-          rescue RSpec::Approvals::ReceivedDiffersError => e
-            # we want to land here and then move on
-          end
-          File.exists?(@approval.received_path).should be_true
-        end
+      it "deletes a previously rejected change from the dotfile" do
+        expected = "spec/approvals/fairy_dust_and_unicorns.received.txt spec/approvals/fairy_dust_and_unicorns.approved.txt"
 
-        it "appends the name to the .approvals file" do
-          begin
-            @approval.verify
-          rescue RSpec::Approvals::ReceivedDiffersError => e
-            # moving on
-          end
-          expected = "spec/approvals/fairy_dust_and_unicorns.received.txt spec/approvals/fairy_dust_and_unicorns.approved.txt"
+        approval.write(:approved, 'abc')
+        begin
+          approval.verify
+        rescue RSpec::Approvals::ReceivedDiffersError => e
+          # guard
           File.read('.approvals').split("\n").should include(expected)
         end
 
-        it "only appends the name once" do
-          begin
-            @approval.verify
-          rescue RSpec::Approvals::ReceivedDiffersError => e
-            # moving on
-          end
+        approval.write(:approved, 'xyz')
+        lambda { approval.verify }.call
+        File.read('.approvals').split("\n").should_not include(expected)
+      end
+    end
 
-          begin
-            @approval.verify
-          rescue RSpec::Approvals::ReceivedDiffersError => e
-            # moving on
-          end
+    context "with a mismatch" do
+      before :each do
+        approval.write(:approved, 'abc')
+      end
 
-          lines = File.read('.approvals').split("\n")
-          lines.size.should eq(lines.sort.uniq.size)
+      it "raises an error" do
+        lambda { approval.verify }.should raise_error(RSpec::Approvals::ReceivedDiffersError)
+      end
+
+      it "leaves a received file" do
+        begin
+          approval.verify
+        rescue RSpec::Approvals::ReceivedDiffersError => e
+          # we want to land here and then move on
         end
+        File.exists?(approval.received_path).should be_true
+      end
+
+      it "appends the name to the .approvals file" do
+        begin
+          approval.verify
+        rescue RSpec::Approvals::ReceivedDiffersError => e
+          # moving on
+        end
+        expected = "spec/approvals/fairy_dust_and_unicorns.received.txt spec/approvals/fairy_dust_and_unicorns.approved.txt"
+        File.read('.approvals').split("\n").should include(expected)
+      end
+
+      it "only appends the name once" do
+        begin
+          approval.verify
+        rescue RSpec::Approvals::ReceivedDiffersError => e
+          # moving on
+        end
+
+        begin
+          approval.verify
+        rescue RSpec::Approvals::ReceivedDiffersError => e
+          # moving on
+        end
+
+        lines = File.read('.approvals').split("\n")
+        lines.size.should eq(lines.sort.uniq.size)
       end
     end
 
     it "fails magnificently" do
-      approval = Approvals::Approval.new(example, 'xyz')
       message = <<-FAILURE_MESSAGE
 
         Approval Failure:
