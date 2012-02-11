9e0b88ac878e0bcc91c97064c547cb1123600d51
Katrina Owen <katrina.owen@gmail.com>
2012-02-11 18:32:22 +0100

    Assign caller in the correct scope

--- lib/rspec/approvals/dsl.rb
+++ lib/rspec/approvals/dsl.rb
@@ -6,13 +6,14 @@ module RSpec
 
       def verify(description, options = {})
 
+        origin = caller
         specify(description) do
           approval = Approval.new(example, yield, options)
 
           # We may be able to set file_path and
           # line_number on example in the approval
           # see RSpec::Core::Metadata::LocationKeys
-          approval.location = caller
+          approval.location = origin
 
           approval.verify
         end
