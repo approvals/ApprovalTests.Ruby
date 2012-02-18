4511ebc2fc392f97ac960ee032088576529f4f11
Katrina Owen <katrina.owen@gmail.com>
2012-02-18 13:02:01 +0100

    Remove extraneous slash in file protocol

--- lib/rspec/approvals/reporters/image_reporter/html_image_reporter.rb
+++ lib/rspec/approvals/reporters/image_reporter/html_image_reporter.rb
@@ -28,7 +28,7 @@ module RSpec
 
       private
       def template(received, approved)
-        "<html><body><center><table style=\"text-align: center;\" border=1><tr><td><img src=\"file:///#{received}\"></td><td><img src=\"file:///#{approved}\"></td></tr><tr><td>received</td><td>approved</td></tr></table></center></body></html>"
+        "<html><body><center><table style=\"text-align: center;\" border=1><tr><td><img src=\"file://#{received}\"></td><td><img src=\"file://#{approved}\"></td></tr><tr><td>received</td><td>approved</td></tr></table></center></body></html>"
       end
     end
 
--- spec/reporters/html_image_reporter_spec.rb
+++ spec/reporters/html_image_reporter_spec.rb
@@ -5,7 +5,7 @@ describe RSpec::Approvals::HtmlImageReporter do
 
   it "creates the appropriate command" do
     result = subject.html("spec/fixtures/one.png", "spec/fixtures/two.png")
-    expected = "<html><body><center><table style=\"text-align: center;\" border=1><tr><td><img src=\"file:////Users/katrina/code/gems/rspec-approvals/spec/fixtures/one.png\"></td><td><img src=\"file:////Users/katrina/code/gems/rspec-approvals/spec/fixtures/two.png\"></td></tr><tr><td>received</td><td>approved</td></tr></table></center></body></html>"
+    expected = "<html><body><center><table style=\"text-align: center;\" border=1><tr><td><img src=\"file:///Users/katrina/code/gems/rspec-approvals/spec/fixtures/one.png\"></td><td><img src=\"file:///Users/katrina/code/gems/rspec-approvals/spec/fixtures/two.png\"></td></tr><tr><td>received</td><td>approved</td></tr></table></center></body></html>"
     if result != expected
       subject.display(result)
     end
