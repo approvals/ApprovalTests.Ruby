9a213971d739fd9e571f0152cf0f0f52cdbd3c45
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-05-23 13:06:29 -0700

    Create a directory namer

--- lib/approvals/extensions/rspec.rb
+++ lib/approvals/extensions/rspec.rb
@@ -1,6 +1,7 @@
 if defined? RSpec
   require 'approvals/extensions/rspec/dsl'
   require 'approvals/namers/rspec_namer'
+  require 'approvals/namers/directory_namer'
 
   RSpec.configure do |c|
     c.include Approvals::RSpec::DSL
