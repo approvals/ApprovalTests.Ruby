d8b30057d48180b85ead53c5f6c61090835f6377
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2013-12-09 08:07:31 -0800

    Allow failure diffs to be configurable globally or per-example

--- lib/approvals/extensions/rspec/dsl.rb
+++ lib/approvals/extensions/rspec/dsl.rb
@@ -12,7 +12,18 @@ module Approvals
         namer = ::RSpec.configuration.approvals_namer_class.new(group.example)
         Approvals.verify(block.call, options.merge(:namer => namer))
       rescue ApprovalError => e
-        ::RSpec::Expectations.fail_with(e.message, e.received_text, e.approved_text)
+        if diff_on_approval_failure?
+          ::RSpec::Expectations.fail_with(e.message, e.received_text, e.approved_text)
+        else
+          raise e
+        end
+      end
+
+      private
+
+      def diff_on_approval_failure?
+        ::RSpec.configuration.diff_on_approval_failure? ||
+          example.metadata[:diff_on_approval_failure]
       end
     end
   end
