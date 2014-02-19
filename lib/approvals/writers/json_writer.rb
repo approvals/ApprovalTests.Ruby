ae407aa1aa4876542fa76ddb769a794c24eac38e
Mark IJbema <markijbema@gmail.com>
2014-02-19 19:06:47 +0100

    Moved filter to file of its own

--- lib/approvals/writers/json_writer.rb
+++ lib/approvals/writers/json_writer.rb
@@ -1,58 +1,12 @@
 module Approvals
   module Writers
     class JsonWriter < TextWriter
-      class Filter
-        attr_reader :filters
-
-        def initialize(filters)
-          @filters = filters
-          @placeholder = {}
-        end
-
-        def apply hash_or_array
-          if @filters.any?
-            censored(hash_or_array)
-          else
-            hash_or_array
-          end
-        end
-
-        def censored value, key=nil
-          case value
-          when Array
-            value.map { |item| censored(item) }
-          when Hash
-            Hash[value.map { |key, value| [key, censored(value, key)] }]
-          else
-            if value.nil?
-              nil
-            elsif key && placeholder_for(key)
-              "<#{placeholder_for(key)}>"
-            else
-              value
-            end
-          end
-        end
-
-        def placeholder_for key
-          return @placeholder[key] if @placeholder.key? key
-
-          applicable_filters = filters.select do |placeholder, pattern|
-            pattern && key.match(pattern)
-          end
-
-          @placeholder[key] = applicable_filters.keys.last
-        end
-      end
-
       def extension
         'json'
       end
 
       def format(data)
-        hash_or_array = parse_data(data)
-
-        hash_or_array = filter.apply(hash_or_array)
+        hash_or_array = filter.apply(parse_data(data))
 
         JSON.pretty_generate(hash_or_array) + "\n"
       end
@@ -69,7 +23,7 @@ module Approvals
       end
 
       def filter
-        Filter.new(Approvals.configuration.excluded_json_keys)
+        ::Approvals::Filter.new(Approvals.configuration.excluded_json_keys)
       end
     end
   end
