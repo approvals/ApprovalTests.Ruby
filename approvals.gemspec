3d6a1d705468a6292e7d1264840d141234237660
Mark IJbema <markijbema@gmail.com>
2014-02-21 17:40:02 +0100

    Fixed simple deprecation warnings

--- approvals.gemspec
+++ approvals.gemspec
@@ -20,7 +20,7 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
 
-  s.add_development_dependency 'rspec', '~> 2.9'
+  s.add_development_dependency 'rspec', '~> 2.99.0.beta2'
   s.add_development_dependency 'json', '~> 1.8'
   s.add_dependency 'thor', '~> 0.18'
   s.add_dependency 'nokogiri', '~> 1.6'
--- spec/system_command_spec.rb
+++ spec/system_command_spec.rb
@@ -4,10 +4,10 @@ require 'approvals/system_command'
 describe Approvals::SystemCommand, "#exists?" do
 
   it "does" do
-    Approvals::SystemCommand.exists?("ls").should be_true
+    Approvals::SystemCommand.exists?("ls").should be_truthy
   end
 
   it "does not" do
-    Approvals::SystemCommand.exists?("absolutelydoesnotexistonyoursystem").should be_false
+    Approvals::SystemCommand.exists?("absolutelydoesnotexistonyoursystem").should be_falsey
   end
 end
