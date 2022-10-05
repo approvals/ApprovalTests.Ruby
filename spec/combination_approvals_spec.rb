0b4df5f93ea81bdd7942fa55e51cddad0cbffec3
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-10-05 21:04:01 +0000

    t test exception in combinations
    Co-authored-by: Sam Livingston-Gray <geeksam@gmail.com>

--- spec/combination_approvals_spec.rb
+++ spec/combination_approvals_spec.rb
@@ -19,4 +19,15 @@ describe Approvals do
     CombinationApprovals.verify_all_combinations(a,b, c,namer:namer, &method(:some_function))
 
   end
+  specify "errors" do 
+    a = ["foo", "bar"]
+    b = [0,1,2]
+    CombinationApprovals.verify_all_combinations(a,b, namer:namer) do |a1,b1| 
+      if b1 == 2
+        raise "There is no 2"
+      end
+      "#{b1} #{a1}s"
+    end
+    
+  end
 end
--- todo.md
+++ todo.md
@@ -1,4 +1,9 @@
-1. Ruby 3
+1. handle namer
+1. use mdsnippets
+
+
+
+
 2. check pending tests
 3. check issues & pull requests
 4. Update dependencies 
