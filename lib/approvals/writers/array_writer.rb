4d347b2be8050ccf9858230d11584cb31c9acf58
Llewellyn Falco <llewellyn.falco@gmail.com>
2025-04-10 00:01:09 +0000

    - B force legacy hash
    Co-Authored-By: Brandon Toone <780144+btoone@users.noreply.github.com>
    Co-Authored-By: Sam Livingston-Gray <geeksam@users.noreply.github.com>

--- lib/approvals/writers/array_writer.rb
+++ lib/approvals/writers/array_writer.rb
@@ -1,17 +1,25 @@
 module Approvals
   module Writers
     class ArrayWriter < TextWriter
+      ORIGINAL_INSPECT = Hash.instance_method(:inspect)
 
       def format(data)
+        Hash.define_method(:inspect) {'{' + map { |k, v| "#{k.inspect}=>#{v.inspect}" }.join(', ') + '}'}
         filter(data).map.with_index do |value, i|
           "[#{i.inspect}] #{value.inspect}\n"
         end.join
+      ensure
+        Hash.define_method(:inspect, ORIGINAL_INSPECT)
       end
 
       def filter data
         filter = ::Approvals::Filter.new(Approvals.configuration.excluded_json_keys)
         filter.apply(data)
       end
+
+      private
+
     end
   end
+
 end
