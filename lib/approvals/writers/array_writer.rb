469a057b1b7c5991c95d6a7dcc9143dcc79a09d6
Llewellyn Falco <llewellyn.falco@gmail.com>
2025-04-16 23:41:16 +0000

    - r cleanup
    Co-Authored-By: Sam Livingston-Gray <geeksam@users.noreply.github.com>

--- lib/approvals/writers/array_writer.rb
+++ lib/approvals/writers/array_writer.rb
@@ -10,38 +10,24 @@ module Approvals
       
       # Add refinements for Array to handle nested hashes
       refine Array do
-        def inspect_with_hash_refinements
-          '[' + map { |v| v.respond_to?(:inspect_with_hash_refinements) ? v.inspect_with_hash_refinements : v.inspect }.join(', ') + ']'
+        def inspect
+          '[' + map { |v| v.inspect }.join(', ') + ']'
         end
       end
       
-      # Add refinements for Object to handle any object type
-      refine Object do
-        def inspect_with_hash_refinements
-          inspect
-        end
-      end
+
     end
 
     class ArrayWriter < TextWriter
       using HashInspectRefinements
       
       # Custom inspect method that uses the refinements
-      def inspect_with_refinements(obj)
-        case obj
-        when Hash
-          obj.inspect  # Uses the refined Hash#inspect
-        when Array
-          obj.inspect_with_hash_refinements
-        else
-          obj.inspect
-        end
-      end
+
 
       def format(data)
         filtered_data = filter(data)
         filtered_data.map.with_index do |value, i|
-          "[#{i.inspect}] #{inspect_with_refinements(value)}\n"
+          "[#{i.inspect}] #{value.inspect()}\n"
         end.join
       end
 
