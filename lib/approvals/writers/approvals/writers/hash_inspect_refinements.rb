020c6b4f5c6ba27cc8f6eb77b80bb7012d2065bb
Llewellyn Falco <llewellyn.falco@gmail.com>
2025-04-16 23:49:02 +0000

    . r move to file
    Co-Authored-By: Sam Livingston-Gray <geeksam@users.noreply.github.com>

---
+++ lib/approvals/writers/approvals/writers/hash_inspect_refinements.rb
@@ -0,0 +1,16 @@
+module Approvals
+  module Writers
+    module HashInspectRefinements
+      refine Hash do
+        def inspect
+          "{#{map { |k, v| "#{k.inspect}=>#{v.inspect}" }.join(', ')}}"
+        end
+      end
+      refine Array do
+        def inspect
+          "[#{map(&:inspect).join(', ')}]"
+        end
+      end
+    end
+  end
+end
--- lib/approvals/writers/array_writer.rb
+++ lib/approvals/writers/array_writer.rb
@@ -1,36 +1,14 @@
 module Approvals
   module Writers
-    # Module containing refinements for custom hash inspection
-    module HashInspectRefinements
-      refine Hash do
-        def inspect
-          '{' + map { |k, v| "#{k.inspect}=>#{v.inspect}" }.join(', ') + '}'
-        end
-      end
-      
-      # Add refinements for Array to handle nested hashes
-      refine Array do
-        def inspect
-          '[' + map { |v| v.inspect }.join(', ') + ']'
-        end
-      end
-      
-
-    end
+    require_relative 'approvals/writers/hash_inspect_refinements.rb'
 
     class ArrayWriter < TextWriter
       using HashInspectRefinements
-      
-      # Custom inspect method that uses the refinements
-
-
       def format(data)
-        filtered_data = filter(data)
-        filtered_data.map.with_index do |value, i|
+        filter(data).map.with_index do |value, i|
           "[#{i.inspect}] #{value.inspect()}\n"
         end.join
       end
-
       def filter(data)
         filter = ::Approvals::Filter.new(Approvals.configuration.excluded_json_keys)
         filter.apply(data)
