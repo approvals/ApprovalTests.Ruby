38a70aa8c30b960313b9d49f08fea4a4e88b3460
Sam Livingston-Gray <geeksam@gmail.com>
2025-04-24 11:40:12 -0700

    . r Remove Singleton from SingletonReporter

--- lib/approvals/reporters/singleton_reporter.rb
+++ lib/approvals/reporters/singleton_reporter.rb
@@ -1,12 +1,6 @@
 module Approvals
   module Reporters
     class SingletonReporter
-      include Singleton
-
-      def self.report(received, approved)
-        instance.report(received, approved)
-      end
-
       def default_launcher
         Launcher.send(launcher_name)
       end
